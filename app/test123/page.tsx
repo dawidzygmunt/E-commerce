c2ed8fcbf9547c2255ed9b7a419e8add28c9c505
unknown <cepix1337@gmail.com>
2023-11-07 21:45:25 +0100

    added new pages

--- app/test123/page.tsx
+++ app/test123/page.tsx
@@ -6,19 +6,9 @@ import { useEffect } from "react";
 
 
 const SetupPage = () => {
-  const onOpen = useStoreModal((state) => state.onOpen)
-  const isOpen = useStoreModal((state) => state.isOpen)
-
-  useEffect(() => {
-    if (!isOpen){
-      onOpen()
-    }
-  }, [isOpen, onOpen])
-
-
   return (
     <div className="p-4">
-      Root pageahhah
+      elo
     </div>
   );
 }
