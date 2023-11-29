d6f1f133d9caa4bd7100b6b77146fa67bf4b5898
Dawid Zygmunt <dawid.zygmunt86@gmail.com>
2023-11-29 12:26:36 +0100

    products fix

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -24,19 +24,19 @@ export const ProductClient:React.FC<ProductClientProps> = ({
     <>
       <div className='flex items-center justify-between'>
         <Heading
-          title={`Billboards (${data.length})`}
-          description='Manage billboards for your store'
+          title={`Products (${data.length})`}
+          description='Manage products for your store'
         />
-        <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
           <Plus className='mr-2 h-4 w-4' />
           Add New
         </Button>
       </div>
       <Separator />
       <DataTable columns={columns} data={data} serachkey="label"/>
-      <Heading title='API' description='Api calls for Billboards'/>
+      <Heading title='API' description='Api calls for Products'/>
       <Separator />
-      <ApiList entityName='billboards' entityIdName="billboardId" />
+      <ApiList entityName='products' entityIdName="productsId" />
     </>
   )
 }
--- app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -1,33 +1,45 @@
 import { format } from "date-fns"
 
-import { BillboardClient } from './components/client'
+import { ProductClient } from './components/client'
 import prismadb from '@/lib/prismadb'
-import { BillboardColum } from './components/columns'
+import { ProductColumn } from './components/columns'
+import { formatter } from "@/lib/utils"
 
 const BillboardsPage = async ({
   params
 }: {
   params: { storeId: string }
 }) => {
-  const billboards = await prismadb.billboard.findMany({
+  const products = await prismadb.product.findMany({
     where: {
       storeId: params.storeId,
+    },
+    include: {
+      category: true,
+      size: true,
+      color: true,
     },
     orderBy: {
       createdAt: 'desc'
     }
   });
 
-  const fromatedBillboards: BillboardColum[] = billboards.map((item) => ({
+  const fromatedProducts: ProductColumn[] = products.map((item) => ({
     id: item.id,
-    label: item.label,
+    name: item.name,
+    isFeatured: item.isFeatured,
+    isArchived: item.isArchived,
+    price: formatter.format(item.price.toNumber()),
+    category: item.category.name,
+    size: item.category.name,
+    color: item.color.value,
     createdAt: format(item.createdAt, "MMMM do, yyyy")
   }));
 
   return (
     <div className='flex-col'>
       <div className='flex-1 space-y-4 p-8 pt-6'>
-        <BillboardClient data={fromatedBillboards} />
+        <ProductClient data={fromatedProducts} />
       </div>
 
     </div>
